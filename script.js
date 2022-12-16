7b9cd2777c1a23c1509a470f88e2677ba356fa4b
Joaquin <joaquin.melone.zarauza@gmail.com>
2022-12-16 10:47:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 // Variables declaration
 
 let firstNumber = 0;
-let secondNumber = 0;
+let secondNumber = 55;
+
+
+// Checks if an operator has been clicked, so we can start storing the new numbers into the secondNumber instead of the first one
+
+let operationState = false;
+
+// Checks the operation that is going to be made
+
+let operatorUsed = "";
 
 // DOM handling
 
@@ -15,17 +24,23 @@ const number7Button = document.querySelector("#num7");
 const number8Button = document.querySelector("#num8");
 const number9Button = document.querySelector("#num9");
 
+const addButton = document.querySelector("#add-operator");
+
+const resultButton = document.querySelector("#final-result");
+
 
 number1Button.addEventListener("click", function() {
     getNumber(firstNumber, 1);
-});
+})
 
-/* number2Button.addEventListener("click", e => {
-    firstNumber = 1;
-    console.log(e.currentTarget.value)
-})  */
+addButton.addEventListener("click", () => {
+    operationState = true;
+    operatorUsed = "add";
+})
 
-
+resultButton.addEventListener("click", function() {
+    firstNumber = operate(operatorUsed, firstNumber, secondNumber);
+})
 
 
 
