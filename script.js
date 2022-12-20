9343764627085c529a10fed5ead62f52fa825baa
Joaquin <joaquin.melone.zarauza@gmail.com>
2022-12-20 20:17:57 -0300

    Update script.js
    -Added delete, AC button and fixed various bugs

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ let numberButtons = [];
 let firstNumber = 0;
 let secondNumber = 0;
 
-
 // Checks if an operator has been clicked, so we can start storing the new numbers into the secondNumber instead of the first one
 
 let operationState = false;
@@ -17,9 +16,18 @@ let operatorUsed = "";
 
 // DOM handling
 
+const screen = document.querySelector("#result-screen");
+
+// Creates the calculator's numbers in the DOM
+
 for(let i = 0; i < 10; i++) {
     numberButtons[i] = document.querySelector(`#num${i}`);
 }
+
+// Calculator's buttons
+
+const deleteButton = document.querySelector("#delete-button");
+const acButton = document.querySelector("#ac-button");
 
 const addButton = document.querySelector("#add-operator");
 const substractButton = document.querySelector("#substract-operator");
@@ -29,20 +37,58 @@ const floatButton = document.querySelector("#float-operator");
 
 const resultButton = document.querySelector("#final-result");
 
+// Creates the eventListeners for the numbers
+ 
 for(let i = 0; i < 10; i++) {
     numberButtons[i].addEventListener("click", function() {
         if (operationState === false) {
             firstNumber = getNumber(firstNumber, i);
+            screen.textContent = `${firstNumber}`;
         }
 
         else if (operationState === true){
             secondNumber = getNumber(secondNumber, i);
+            screen.textContent = `${secondNumber}`;
         }
     })
 }
 
 
+deleteButton.addEventListener("click", () => {
 
+
+    if (operationState === false && (firstNumber >= -9 && firstNumber < 10)) {
+        firstNumber = 0;
+        screen.textContent = `${firstNumber}`;
+    }
+
+    else if (operationState === false && (firstNumber > 9 || firstNumber < -9)) {
+        firstNumber = removeNumber(firstNumber);
+        screen.textContent = `${firstNumber}`;
+    }
+
+    else if (operationState === true && (secondNumber >= -9 && secondNumber < 10)) {
+        secondNumber = 0;
+        screen.textContent = `${secondNumber}`;
+    }
+
+    else if (operationState === true && (secondNumber > 9 || secondNumber < -9)){
+        secondNumber = removeNumber(secondNumber);
+        screen.textContent = `${secondNumber}`;
+    }
+
+    
+})
+
+acButton.addEventListener("click", () => {
+    firstNumber = 0;
+    secondNumber = 0;
+
+    screen.textContent = `${firstNumber}`;
+})
+
+
+// Operators
 
 addButton.addEventListener("click", () => {
     operationState = true;
@@ -70,32 +116,38 @@ floatButton.addEventListener("click", () => {
 
 
 
+// Result
+
+
 resultButton.addEventListener("click", function() {
     firstNumber = operate(operatorUsed, firstNumber, secondNumber);
+    operationState = false;
+    screen.textContent = `${firstNumber}`; 
     secondNumber = 0;
 })
 
 
 
-
-
-
-
+// Functions
 
 function add(a, b) {
-    return a + b;
+    let tempNumber = (a + b) * 100;
+    return tempNumber = (Math.round(tempNumber)) / 100;
 }
 
 function substract(a, b) {
-    return a - b;
+    let tempNumber = (a - b) * 100;
+    return tempNumber = (Math.round(tempNumber)) / 100;
 }
 
 function multiply(a, b) {
-    return a * b;
+    let tempNumber = (a * b) * 100;
+    return tempNumber = (Math.round(tempNumber)) / 100;
 }
 
 function divide(a, b) {
-    return a / b;
+    let tempNumber = (a / b) * 100;
+    return tempNumber = (Math.round(tempNumber)) / 100;
 }
 
 function operate(operator, a, b) {
@@ -135,5 +187,13 @@ function getNumber(storedNumber, newNumber) {
 
         return number;
     }
-    
+}
+
+function removeNumber(storedNumber) {
+    storedNumber = storedNumber.toString();
+    storedNumber = storedNumber.slice(0, -1);
+
+    number = parseFloat(storedNumber);
+
+    return number;
 }
